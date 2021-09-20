343d904635256cde36ae47d15f91dc55b3e47a7b
Dylan Guereca <guereca.caceres@gmail.com>
2021-09-20 08:35:28 -0400

    Terminando el código

--- src/main/java/bo/edu/ucb/est/Bot.java
+++ src/main/java/bo/edu/ucb/est/Bot.java
@@ -26,15 +26,15 @@ public class Bot extends TelegramLongPollingBot {
         if(update.hasMessage()){
             //creo el objeto para enviar un mensaje
             SendMessage message = new SendMessage();
-            message.set.ChatId(update.getMessage().getChatId().toString());//Define a quien le vamos a enviar el mensaje
-            message.set.Text("Bienbeninido al Bot Calculadora.\\n\" +\"Seleccione una de las siguientes opciones:\\n\" +\"1. Sumar dos números.\\n\" +\"2. Calcular serie de fibonacci.");
+            message.setChatId(update.getMessage().getChatId().toString());//Define a quien le vamos a enviar el mensaje
+            message.setText("Bienveninido al Bot Calculadora.\n Seleccione una de las siguientes opciones:\n 1. Sumar dos números.\n 2. Calcular serie de fibonacci.");
             try{
                 execute(message); // Envia el mensaje
                 if(update.hasMessage()) { // Verificamos que tenga mensaje
                     SendMessage message1 = new SendMessage();
-            
                     SendMessage message3 = new SendMessage();
                     message1.setChatId(update.getMessage().getChatId().toString());
+                    message3.setChatId(update.getMessage().getChatId().toString());
                     if(update.getMessage().getText().toString().equals("1")){
                         int a;
                         int b;
@@ -44,11 +44,37 @@ public class Bot extends TelegramLongPollingBot {
                         try{
                             execute(message1);
                             if(update.hasMessage()){
-                                
+                                SendMessage message2 = new SendMessage();
+                                message2.setChatId(update.getMessage().getChatId().toString());
+                                message2.setText("Ingrese segundo número");
+                                b = Integer.parseInt(update.getMessage().getText());
+                                try{
+                                    execute (message2);
+                                    c = a+b;
+                                    message3.setText("La suma de "+a+" + "+b+" es "+c);
+                                    try{
+                                        execute (message3);
+                                    }catch(TelegramApiException e){
+                                        e.printStackTrace();
+                                    }
+                                }catch(TelegramApiException e){
+                                   e.printStackTrace(); 
+                                }
                             }
                         }catch(TelegramApiException e){
                             e.printStackTrace();
                         }
+                    }else{
+                       if(update.getMessage().getText().toString().equals("2")){
+                           SendMessage message4 = new SendMessage();
+                           message4.setChatId(update.getMessage().getChatId().toString());
+                           message4.setText("Funcionalidad no implementada, intente otro día");
+                           try {
+                                execute(message4);
+                           }catch(TelegramApiException e){
+                                e.printStackTrace();
+                           }
+                       } 
                     }
                 }
             }catch(TelegramApiException e){
